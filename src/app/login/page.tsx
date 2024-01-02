17364f1ef12dbe267f5fe318d94026323bc1d1c3
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2024-01-02 13:36:26 +0530

    add login function

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,75 +1,102 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
   const [user, setUser] = useState({ email: "", password: "" });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const inputClass =
+    "sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500";
 
-  const onLogin = () => {};
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
     <section className="bg-gray-900 min-h-screen flex flex-col items-center justify-center">
       <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
-            Login into your account
+            {loading ? "Processing..." : "Login"}
           </h1>
 
-          <form className="space-y-4 md:space-y-6" onSubmit={onLogin}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-white"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={user.email}
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                className=" sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="name@company.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                className=" sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 bg-gray-900"
+          <div className="space-y-4 md:space-y-6">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-white"
             >
-              Login
-            </button>
-            <p className="text-sm font-light text-gray-400">
-              Don't have an account?{" "}
-              <Link
-                href="/signup"
-                className="font-medium hover:underline text-primary-500"
-              >
-                Signup here
-              </Link>
-            </p>
-          </form>
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              className={inputClass}
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className={inputClass}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 bg-gray-900"
+            onClick={onLogin}
+          >
+            Login
+          </button>
+          <p className="text-sm font-light text-gray-400">
+            Don't have an account?{" "}
+            <Link
+              href="/signup"
+              className="font-medium hover:underline text-primary-500"
+            >
+              Signup here
+            </Link>
+          </p>
         </div>
       </div>
     </section>
